2ffc8106c444301c35eacfb33d5e9117e491feba
Dahee96 <57829246+Dahee96@users.noreply.github.com>
2025-01-07 17:17:59 +0900

    Update plot_stft.py

--- plot_stft.py
+++ plot_stft.py
@@ -38,7 +38,7 @@ def plot_waveform_to_numpy(self, y, y_low, y_recon, step):
         # plt.imshow(rosa.amplitude_to_db(self.stftmag(yy).numpy(),
         #                                 ref=np.max, top_db=80.),
         #            # vmin = -20,
-        #            vmax=0.,
+
 
 
 
