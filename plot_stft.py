adb45f23892d547c5618c8a5c94d6325e8694072
Dahee96 <57829246+Dahee96@users.noreply.github.com>
2025-01-07 17:12:13 +0900

    Update plot_stft.py

--- plot_stft.py
+++ plot_stft.py
@@ -12,8 +12,6 @@ import matplotlib.gridspec as gridspec
 # input_unmasked_path = '/home/utahboy3502/1007MIR/MIR/output/'
 # wav_original_name = 'test1.wav'
 # unmasked_wav_name = 'unmasked_output.wav'
-# masked_wav_name = 'kf99_output.wav'
-
 # outputpath = '/home/utahboy3502/1007MIR/MIR/output/'
 
 input_original_path = '/home/donghyun/Research/TUNet/test/version_7/sample_1/'
