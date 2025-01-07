becb17e35da24f1cb4b96622f7d924aa45f864dd
Dahee96 <57829246+Dahee96@users.noreply.github.com>
2025-01-07 17:11:33 +0900

    Update polqa.py

--- polqa.py
+++ polqa.py
@@ -32,8 +32,7 @@ def evaluate_dataset(input_clean_path, input_enhanced_path):
     for i in tqdm(range (800)):
         polqa = compute_audio_quality(metrics='POLQA',testFile=lr_file_list[i],refFile=hr_file_list[i])
         print(polqa)
-     
-
+    
 
 if __name__ == "__main__":
 
