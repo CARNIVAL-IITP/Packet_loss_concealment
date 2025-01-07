a4e020e0d6d0087229479377cfcecde581ec6abc
Dahee96 <57829246+Dahee96@users.noreply.github.com>
2025-01-07 17:08:18 +0900

    Update main_propose.py

--- main_propose.py
+++ main_propose.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
         train()
     else:
         model = resume(None, None, args.version)
-        print(model.hparams)
+        
         print(summarize(model))
 
         model.eval()
