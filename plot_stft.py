2bb6e14a01a22c4af56a036844ecaf996a222bc8
Dahee96 <57829246+Dahee96@users.noreply.github.com>
2025-01-07 17:14:29 +0900

    Update plot_stft.py

--- plot_stft.py
+++ plot_stft.py
@@ -14,7 +14,7 @@ input_unmasked_path = '/home/donghyun/Research/TUNet/test/version_7/sample_1/'
 input_masked_path = '/home/donghyun/Research/TUNet/test/version_7/sample_1/'
 input_masked_path2 = '/home/donghyun/Research/audio-super-res/data/vctk/VCTK-Corpus/test_single/mask_dental/tfilm_e50/'
 
-## Linux filepath
+##
 # input_unmasked_path = '/home/donghyun/Research/audio-super-res/data/vctk/VCTK-Corpus/test_single/mask_dental/lr_inference2/'
 # input_masked_path = '/home/donghyun/Research/audio-super-res/data/vctk/VCTK-Corpus/test_single/mask_dental/tfilm_mae_e100/inference2/train2_lpf_notched/'
 # input_masked_path2 = '/home/donghyun/Research/audio-super-res/data/vctk/VCTK-Corpus/test_single/mask_dental/tfilm_e50/'
