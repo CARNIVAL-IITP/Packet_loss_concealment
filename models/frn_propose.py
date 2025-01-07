d920560f969216f10167ceacdb5ff751b87219ea
Dahee96 <57829246+Dahee96@users.noreply.github.com>
2025-01-07 17:00:44 +0900

    Update frn_propose.py

--- models/frn_propose.py
+++ models/frn_propose.py
@@ -44,8 +44,6 @@ class PLCModel(pl.LightningModule):
             # self.predictor = Predictor.load_from_checkpoint(pred_ckpt_path)
             self.RI_predictor = RI_Predictor.load_from_checkpoint(pred_ckpt_path)
         else:
-            # self.predictor = Predictor(window_size=self.window_size, lstm_dim=self.pred_dim,
-            #                            lstm_layers=self.pred_layers)
             self.RI_predictor = RI_Predictor(window_size=self.window_size, lstm_dim=self.pred_dim, lstm_layers=self.pred_layers)
 
         self.joiner = nn.Sequential(
