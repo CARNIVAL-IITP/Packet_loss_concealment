8be001e4a7ffac0a1ef8fd3c24be8dde6ff2d42c
Dahee96 <57829246+Dahee96@users.noreply.github.com>
2025-01-07 17:15:16 +0900

    Update plot_stft.py

--- plot_stft.py
+++ plot_stft.py
@@ -15,7 +15,7 @@ input_masked_path = '/home/donghyun/Research/TUNet/test/version_7/sample_1/'
 input_masked_path2 = '/home/donghyun/Research/audio-super-res/data/vctk/VCTK-Corpus/test_single/mask_dental/tfilm_e50/'
 
 
-# unmasked_wav_name = 'p225_355.wav' 
+
 # masked_wav_name = 'p225_355.wav' 
 # masked_wav_name2 = '0.2.pr.wav'
 
